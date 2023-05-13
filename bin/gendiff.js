bbde7936e9f4a41f0919119d7e75a3f1b3bfc664
Aleksandr <aleksandr.evsyunin>
2023-05-13 15:56:20 +0600

    2 exercise

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
+const command =(filepath1,filepath2) =>{
+
+};
 program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference')
-.version('1.0.0');
-
+.version('1.0.0')
+.arguments('<filepath1> <filepath2>')
+.action(command)
+.option('-f, --format <type>', 'output format')
 program.parse();
+
 
