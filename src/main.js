845f321a9f960976b172d83349de94768b2d974f
Aleksandr <aleksandr.evsyunin>
2023-05-27 20:01:09 +0600

    change main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,23 @@ import _ from 'lodash';
 import yaml from 'js-yaml';
 // const fs = require('fs');
 
+// Функция для проверки сущ. ключа в объекте
+const hasKey = (obj, key) => key in obj;
+
+// Функция вывода уникальных ключей из объектов
+const getUniqKeysFromObj = (obj1, obj2) => {
+  const keys = Object.keys(obj1).concat(Object.keys(obj2));
+  const uniqKeys = _.uniq(keys);
+  const sortedKeys = _.sortBy(uniqKeys);
+  return sortedKeys;
+};
+
 const compareFunction = (obj1, obj2) => {
-  const getAllKeysOfObj = Object.keys(obj1).concat(Object.keys(obj2));
-  const keys = _.sortBy(_.uniq(getAllKeysOfObj));
+  const keys = getUniqKeysFromObj(obj1, obj2);
   const result = keys.map((key) => {
     let str;
-    const isKeyInObj1 = key in obj1;
-    const isKeyInObj2 = key in obj2;
+    const isKeyInObj1 = hasKey(obj1, key);
+    const isKeyInObj2 = hasKey(obj2, key);
     // есть в 1 нет во 2
     if (isKeyInObj1 && !isKeyInObj2) {
       str = `  - ${key}: ${obj1[key]}`;
